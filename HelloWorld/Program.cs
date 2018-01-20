467b1c714b4604cac8a654e8d79e51d45bf948b9
Amin Hassan <aminh@uw.edu>
2018-01-20 15:52:19 -0800

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keeps program open until pressed any key
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+         
+        }
+    }
+}
